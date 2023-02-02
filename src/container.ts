3977e68bba4c4f7c17bbbf06db69d8b8b73b7d4a
Guillaume (Goliver) Golay <goliworks@gmail.com>
2023-02-02 18:55:48 +0100

    feat: display frames

--- src/container.ts
+++ src/container.ts
@@ -2,18 +2,40 @@ import {LsReader} from "./ls-reader";
 
 export default class Container extends HTMLElement {
 
-    frames: string[] = [];
+    frames: HTMLImageElement[] = [];
+    currentFrame = 0;
+    imgFrame: HTMLDivElement;
 
     constructor() {
         super();
-        this.innerHTML = "<span>Playmics container</span>";
-
+        // this.innerHTML = "<span>Playmics container</span>";
         const lsUrl = this.getAttribute("src");
+        this.imgFrame = document.createElement("div");
+        this.imgFrame.setAttribute("id", "img-frame");
+        this.appendChild(this.imgFrame);
         if (lsUrl) {
-            LsReader.parse(lsUrl).then(frames => {
-                this.frames = frames;
-                console.log(this.frames);
-            });
+            this.preloadImages(lsUrl);
         }
+    }
+
+    private preloadImages(url: string) {
+        LsReader.parse(url).then(frames => {
+            frames.forEach((e, i) => {
+                const img = new Image();
+                img.src = e;
+                img.onload = () => {
+                    if (frames.length - 1 === i) {
+                        console.log(this.frames)
+                        this.displayFrame();
+                    }
+                }
+                this.frames.push(img);
+            })
+        });
+    }
+
+    private displayFrame() {
+        console.log(this.frames[this.currentFrame].src);
+        this.imgFrame.style.backgroundImage = `url(${this.frames[this.currentFrame].src})`;
     }
 }
