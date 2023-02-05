8b5fa94792ca70cc180d35cdea7b866e6fbd12bc
Guillaume (Goliver) Golay <goliworks@gmail.com>
2023-02-05 20:48:08 +0100

    feat: phone icon animation

--- src/orientation-message.ts
+++ src/orientation-message.ts
@@ -2,7 +2,17 @@ export default class OrientationMessage extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-            <span>Please, change screen orientation to landscape</span>
+<!--            <span>Please, change screen orientation to landscape</span>-->
+            <div class="phone-icon">
+                <div class="arrow">
+                    <div class="arrow-line"></div>
+                    <div class="arrow-head"></div>
+                </div>
+                <div class="phone">
+                    <div class="phone-glass"></div>
+                    <div class="phone-button"></div>
+                </div>
+            </div>
         `;
     }
 }
